351ff0a9fed86e0f57e1602afa6e468e7d997185
DESKTOP-GYUNAM <kimkunam06@gmail.com>
2024-11-26 15:22:45 +0900

    🔧 [24-11-26 화] 5교시 Spring - Security (AuthDetails) - [로그인 / 로그아웃] 매우 어려움.

--- chap01-session-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/config/SecurityConfig.java
+++ chap01-session-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.ohgiraffers.sessionsecurity.config;
 
 import com.ohgiraffers.sessionsecurity.common.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 /* comment. spring security 기능 활성화를 위한 어노테이션 */
@@ -25,12 +29,22 @@ public class SecurityConfig {
         // 나중에 BCryptPasswordEncoder 별로라면 다른 거 상속 받으면됨
     }
 
+    // 정적 리소스에 대한 요청은 시큐리티 설정이 돌지 못하게 설정
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer () {
+        return web ->
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    // 로그인 실패했을 때 핸들링 할 메서드 생성
+    @Autowired
+    private AuthFailHandler authFailHandler;
 
     /* comment. 이 부분이 설정의 핵심!!!! */
     // 요청에 대한 컨트롤 로직 작성
     // 로그인 필요하면 얘가 위의 URL로 가라고 필터를 걸어놓는 식.
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http){
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // 서버의 리소스 접근 가능 권한 설정
         http.authorizeHttpRequests(auth -> {
         // permitAll() → 인증되지 않은(로그인 되지 않은) 사용자들이 접근할 수 있는 URL 기술
@@ -48,7 +62,32 @@ public class SecurityConfig {
             login.loginPage("auth/login"); // 로그인 페이지 url을 기술
             // 사용자가 ID를 입력하는 필드(input 타입 name과 반드시 일치해야 함)
             login.usernameParameter("user"); // 여기에 form 태그의 name(id) 속성 입력
-        });
+            // 사용자가 PWD를 입력하는 필드 (input 타입 name과 반드시 일치)
+            login.passwordParameter("pass"); // 여기에 form 태그의 name(pass) 속성 입력
+            // 사용자가 로그인에 성공했을 시 보내줄 URL 기술
+            login.defaultSuccessUrl("/", true); // 메인으로 보내줌
+            // 로그인에 실패했을 시 내용을 기술한 객체, 호출 아직 미작성
+            login.failureHandler(authFailHandler);
+        }).logout(logout -> {
+            // 로그아웃을 담당할 핸들러 메서드 요청 URL 기술
+            logout.logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"));
+            /* session은 쿠키 방식으로 저장되어 있어, 로그인 시
+                해당하는 쿠키를 삭제함으로써 로그아웃을 만들어 줌. */
+            logout.deleteCookies("JSESSIONID");
+            // 서버측의 세선 공간 만료
+            logout.invalidateHttpSession(true);
+            // 로그아웃 성공 시 요청 URL 기술
+            logout.logoutSuccessUrl("/");
+        }).sessionManagement(session -> {
+            session.maximumSessions(1); // session의 허용 개수 제한
+            // 한 사용자가 여러 창을 띄워 동시에 세션 여러개 활성화 방지
 
+            // 세션이 만료 되었을 때 요청할 URL 기술
+            session.invalidSessionUrl("/");
+            // 추가적인 구현이 필요하므로 비활성화 (나중에 다시 강의)
+        }).csrf(csrf -> csrf.disable());
+
+        // 위에서 설정한 내용대로 시큐리티 기능 빌드 (생성)
+        return http.build();
     }
 }
